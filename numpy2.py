926678fa14b5ce68c67f5ad8c9be0df78b552aeb
aman2909-creator <amandeepsinghpathania7@gmail.com>
2025-05-21 10:30:00 +0530

    Element-wise Operations

---
+++ numpy2.py
@@ -0,0 +1,14 @@
+# Create two arrays:
+# Perform:
+# Element-wise addition, subtraction
+# Element-wise multiplication
+# Dot product of a and b
+
+
+import numpy as np
+a = np.array([10,20,30,40])
+b = np.array([3,6,8,22])
+print(a+b)
+print(a-b)
+print(a*b)
+print(a.dot(b))
