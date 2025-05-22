a3c26eff7509197b1db9a98f337aa2178c8c8058
aman2909-creator <amandeepsinghpathania7@gmail.com>
2025-05-22 09:52:02 +0530

    practicing pandas

---
+++ pandas1.py
@@ -0,0 +1,46 @@
+import numpy as np 
+import pandas as pd
+
+a = pd.date_range("20250522",periods=12)
+# print(a)
+
+# df = pd.DataFrame({"date":['20250522','20250524','20250526'],"value":[10,20,30]})
+
+# df['date'] = pd.to_datetime(df["date"])  
+# df.set_index('date',inplace = True)  
+# print(df.resample('D').mean())                                                         
+# print(df.resample('D').sum())                                                         
+# print(df)
+# print(b)
+# df = pd.DataFrame({"A":1.0,"B":pd.Timestamp("20250522"),"C":pd.Series(1,index=list(range(4))),"D":np.array([3]*4),"E":pd.Categorical(["test","train","test","train"]),"F":"foo"})
+df = pd.DataFrame(np.random.rand(12,4),index = a, columns=list("ABCD"))
+# print(df.head())
+# print(df.tail())
+# print(df.index)
+# print(df.columns)
+# print(df.to_numpy())
+# print(df.describe())
+# print(df.T)
+# print(df.sort_index(axis=1,ascending=False))
+# print(df.sort_values(by='B'))
+
+# Getitem
+# print(df['A'])
+# print(df[0:3])
+
+
+# Selection by label
+# print(df.loc[a[0]])
+# print(df.loc[:,["A","B"]])
+# print(df.loc[[a[0]],["A"]])
+# print(df.at[a[0],"A"])
+
+
+
+# Selection by position
+# print(df.iloc[3])
+# print(df.iloc[0:3,0:2])
+# print(df.iloc[[1, 2, 4], [0, 2]])
+# print(df.iloc[1:3, :])
+# print(df.iloc[1,1])
+# print(df.iat[1,1])
